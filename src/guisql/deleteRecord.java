c1634df2eb251e03b171857309edb54ffa8102bb
downesk4 <34350207+downesk4@users.noreply.github.com>
2018-03-29 19:43:14 +0100

    class for deleting

---
+++ src/guisql/deleteRecord.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guisql;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author FiercePC
+ */
+public class deleteRecord {
+    
+    public void cheeseDelete()
+    {
+        JOptionPane.showMessageDialog(null, "Cheese Deleted");
+    }
+    
+    public void cheeseBoardDelete()
+    {
+        JOptionPane.showMessageDialog(null, "CheeseBoard Deleted");
+    }
+    
+    public void linkDelete()
+    {
+        JOptionPane.showMessageDialog(null, "Link record Deleted");
+    }
+    
+}
