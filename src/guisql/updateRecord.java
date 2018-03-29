ae25e8ad46cb6722017c9d64f8df6e164aa8c4e7
downesk4 <34350207+downesk4@users.noreply.github.com>
2018-03-29 19:53:01 +0100

    class for updating

---
+++ src/guisql/updateRecord.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guisql;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author FiercePC
+ */
+public class updateRecord {
+    
+    public void cheeseUpdate()
+    {
+        JOptionPane.showMessageDialog(null, "Cheese Updated");
+    }
+    
+    public void cheeseBoardUpdate()
+    {
+        JOptionPane.showMessageDialog(null, "CheeseBoard Updated");
+    }
+    
+    public void linkUpdate()
+    {
+        JOptionPane.showMessageDialog(null, "Link record Updated");
+    }
+}
