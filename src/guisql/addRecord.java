97205f39b4af85273fb13c63544734ab6d9abfdc
downesk4 <34350207+downesk4@users.noreply.github.com>
2018-03-29 19:59:56 +0100

    class for adding a record

---
+++ src/guisql/addRecord.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guisql;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author FiercePC
+ */
+public class addRecord {
+    
+    public void cheeseAdd()
+    {
+        JOptionPane.showMessageDialog(null, "Cheese Added");
+    }
+    
+    public void cheeseBoardAdd()
+    {
+        JOptionPane.showMessageDialog(null, "CheeseBoard Added");
+    }
+    
+    public void linkAdd()
+    {
+        JOptionPane.showMessageDialog(null, "Link record Added");
+    }
+    
+}
